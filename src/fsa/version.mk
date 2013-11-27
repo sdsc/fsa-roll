a3b3fe7f46eb336976c254d4a534d06ce1f4016d
Trevor Cooper <tcooper@sdsc.edu>
2013-11-26 16:52:36 -0800

    Updated Makefile and version.mk to use pull.mk

--- src/fsa/version.mk
+++ src/fsa/version.mk
@@ -1,3 +1,14 @@
-NAME    = fsa
-VERSION = 1.15.7
-RELEASE = 1
+NAME            = fsa
+VERSION         = 1.15.7
+RELEASE         = 1
+PKG_ROOT        = /opt/fsa
+
+SRC_SUBDIR      = fsa
+
+FSA_NAME        = $(NAME)
+FSA_VERSION     = $(VERSION)
+FSA_PKG_SUFFIX  = tgz
+FSA_SOURCE_PKG  = $(FSA_NAME)-$(FSA_VERSION).$(FSA_PKG_SUFFIX)
+FSA_SOURCE_DIR  = $(FSA_SOURCE_PKG:%.$(FSA_PKG_SUFFIX)=%)
+
+TGZ_PKGS         = $(FSA_SOURCE_PKG)
