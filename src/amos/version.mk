c24104828ed5813ddf3019b437ae25840f408585
Trevor Cooper <tcooper@sdsc.edu>
2013-11-26 16:52:30 -0800

    Updated Makefile and version.mk to use pull.mk

--- src/amos/version.mk
+++ src/amos/version.mk
@@ -1,3 +1,14 @@
-NAME    = amos
-VERSION = 3.1.0
-RELEASE = 1
+NAME             = amos
+VERSION          = 3.1.0
+RELEASE          = 1
+PKG_ROOT         = /opt/amos
+
+SRC_SUBDIR       = amos
+
+AMOS_NAME        = $(NAME)
+AMOS_VERSION     = $(VERSION)
+AMOS_PKG_SUFFIX  = tgz
+AMOS_SOURCE_PKG  = $(AMOS_NAME)-$(AMOS_VERSION).$(AMOS_PKG_SUFFIX)
+AMOS_SOURCE_DIR  = $(AMOS_SOURCE_PKG:%.$(AMOS_PKG_SUFFIX)=%)
+
+TGZ_PKGS         = $(AMOS_SOURCE_PKG)
