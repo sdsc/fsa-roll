eeebc0360e7fedbdbf8b65556e2c1747e790c714
Trevor Cooper <tcooper@sdsc.edu>
2013-11-26 16:52:40 -0800

    Updated Makefile and version.mk to use pull.mk

--- src/mummer/version.mk
+++ src/mummer/version.mk
@@ -1,3 +1,14 @@
-NAME    = mummer
-VERSION = 3.23
-RELEASE = 1
+NAME               = mummer
+VERSION            = 3.23
+RELEASE            = 1
+PKG_ROOT           = /opt/mummer
+
+SRC_SUBDIR         = mummer
+
+MUMMER_NAME        = $(NAME)
+MUMMER_VERSION     = $(VERSION)
+MUMMER_PKG_SUFFIX  = tar.gz
+MUMMER_SOURCE_PKG  = $(MUMMER_NAME)-$(MUMMER_VERSION).$(MUMMER_PKG_SUFFIX)
+MUMMER_SOURCE_DIR  = $(MUMMER_SOURCE_PKG:%.$(MUMMER_PKG_SUFFIX)=%)
+
+TAR_GZ_PKGS        = $(MUMMER_SOURCE_PKG)
