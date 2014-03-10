c2b30a30ac74b11edff8f1f81a1f2a26705e706e
Jim Hayes <jhayes@sdsc.edu>
2014-03-10 12:13:25 -0700

    Correct package extension.

--- src/fsa/version.mk
+++ src/fsa/version.mk
@@ -2,13 +2,14 @@ NAME            = fsa
 VERSION         = 1.15.7
 RELEASE         = 1
 PKG_ROOT        = /opt/fsa
+RPM.EXTRAS         = AutoReq:No
 
 SRC_SUBDIR      = fsa
 
 FSA_NAME        = $(NAME)
 FSA_VERSION     = $(VERSION)
-FSA_PKG_SUFFIX  = tgz
+FSA_PKG_SUFFIX  = tar.gz
 FSA_SOURCE_PKG  = $(FSA_NAME)-$(FSA_VERSION).$(FSA_PKG_SUFFIX)
 FSA_SOURCE_DIR  = $(FSA_SOURCE_PKG:%.$(FSA_PKG_SUFFIX)=%)
 
-TGZ_PKGS         = $(FSA_SOURCE_PKG)
+TAR_GZ_PKGS         = $(FSA_SOURCE_PKG)
